7039e04e1cc57e7f9693acd05ab04ed6f9b5492e
Anna De Lise <anna_delise@libero.it>
2025-07-30 14:51:59 +0200

    read comment

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -1,20 +1,19 @@
 import { useEffect, useState } from "react";
-import { useParams } from "react-router-dom"
-import { Alert, Spinner, Container, Row, Col, Image } from "react-bootstrap";
+import { useParams } from "react-router-dom";
+import { Alert, Spinner, Container, Row, Col, Image, ListGroup } from "react-bootstrap";
 
+const MovieDetails = () => {
+  const [readFilm, setReadFilm] = useState();
+  const [comm, setComm] = useState([]);
+  const [isError, setIsError] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const params = useParams();
 
-const MovieDetails =()=> {
-
-    const[readFilm, setReadFilm] = useState();
-const[isError, setIsError]=useState(false)
-const[isLoading, setIsLoading]= useState(true)
-    const params = useParams();
-
-    const endpoint = "https://www.omdbapi.com/?apikey=";
-    const key = "f5942648";
-    const chiamata = (id)=>{
-        fetch(endpoint + key + "&i=" + id)
-         .then((response) => {
+  const endpoint = "https://www.omdbapi.com/?apikey=";
+  const key = "f5942648";
+  const chiamataFilm = (id) => {
+    fetch(endpoint + key + "&i=" + id)
+      .then((response) => {
         if (response.ok) {
           return response.json();
         } else {
@@ -22,36 +21,96 @@ const[isLoading, setIsLoading]= useState(true)
         }
       })
       .then((data) => {
-        setReadFilm(data)
-        setIsLoading(false)
+        setReadFilm(data);
+        setIsLoading(false);
       })
       .catch((err) => {
-        setIsError(true)
-        setIsLoading(false)
+        setIsError(true);
+        setIsLoading(false);
         console.error(err);
       });
-    }
+  };
 
-    useEffect(()=>{
-chiamata(params.movieId)
-    },[])
-    return(
-        <>
-         {isError && ( <Alert variant="danger">Impossibile recuperare il film...</Alert>)}
-        {isLoading && (<div className="d-flex justify-content-center">
-    <Spinner animation="border" variant="light" />
-  </div>) }
-        {!isLoading && !isError && (
-        <Container fluid>
-            <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-4 row-cols-xl-6 px-4 ">
-                <Col key={readFilm.imdbID}  className="mb-2 text-center px-1 d-flex flex-column h-100">
-                  <Image fluid
-                    src={readFilm.Poster} alt={`Copertina film: ${readFilm.Title}`} 
-                    style={{ objectFit: 'cover', height: '400px' }}
-                    />
-                </Col>
-            </Row>
-          </Container>)}
-    </>)
-}
-export default MovieDetails
+  const endopint2 = `https://striveschool-api.herokuapp.com/api/comments/`;
+  const chiamataCommenti = (id) => {
+    fetch(endopint2 + id, {
+  headers: {
+    Authorization:
+      'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2ODgyMWYzMmZlMzZkMDAwMTU5NzU4MTYiLCJpYXQiOjE3NTMzNTgxMzAsImV4cCI6MTc1NDU2NzczMH0.e0s40I7Kn-R4ZbRhY2HdP17MOyFqP76G-kLpdCuZ1Xs',
+  },
+})
+
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        } else {
+          throw new Error(`Errore richiesta: ${response.status}`);
+        }
+      })
+      .then((data) => {
+        setComm(data);
+        console.log(data)
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  useEffect(() => {
+    chiamataFilm(params.movieId);
+    chiamataCommenti(params.movieId);
+  }, []);
+  return (
+    <>
+      {isError && (
+        <Alert variant="danger">Impossibile recuperare il film...</Alert>
+      )}
+      {isLoading && (
+        <div className="d-flex justify-content-center">
+          <Spinner animation="border" variant="light" />
+        </div>
+      )}
+      {!isLoading && !isError && (
+        <Container fluid style={{ backgroundColor: "#141414" }}>
+          <Row className="row-cols-1 row-cols-sm-2 row-cols-lg-3 row-cols-xl-3 px-4 justify-content-center ">
+            <Col
+              key={readFilm.imdbID}
+              className="mb-2 text-center px-1 d-flex flex-column h-100">
+              <Image
+                fluid
+                src={readFilm.Poster}
+                alt={`Copertina film: ${readFilm.Title}`}
+                style={{ objectFit: "cover", height: "700px" }}
+              />
+            </Col>
+            <Col>
+              <h2 className="text-white">{readFilm.Title}</h2>
+              <h6 className="text-secondary">
+                {readFilm.Year} - {readFilm.Genre}
+              </h6>
+              <h5 className="text-white fw-normal">{readFilm.Plot}</h5>
+             {comm.length > 0 && (
+  <div className="mt-4">
+    <h5 className="text-white">Recensioni</h5>
+    <ListGroup>
+      {comm.map((commento) => (
+        <ListGroup.Item
+          key={commento._id}
+          className="bg-dark text-light border-secondary"
+        >
+         <h6>{commento.author}</h6>
+         <p>{commento.comment}</p>
+        <h6>{commento.rate}/5</h6> 
+        </ListGroup.Item>
+      ))}
+    </ListGroup>
+  </div>
+)}
+            </Col>
+          </Row>
+        </Container>
+      )}
+    </>
+  );
+};
+export default MovieDetails;
